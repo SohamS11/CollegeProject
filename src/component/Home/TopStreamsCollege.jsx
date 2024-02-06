233d7b88cdb40b55069b4815c806433538627bdb
Tochage143 <rbactionmovies@gmail.com>
2024-02-06 16:15:17 +0530

    college list route add by rohit

--- src/component/Home/TopStreamsCollege.jsx
+++ src/component/Home/TopStreamsCollege.jsx
@@ -3,25 +3,24 @@ import { TopStreamsCollegeData } from "../../Data/TopStreamsCollegeData";
 import { useState } from "react";
 import { BsDot } from "react-icons/bs";
 
-
 const TopStreamsCollege = () => {
   const [currentData, setCurrentData] = useState(3);
- // This currentdata store which array to show on the screen 
+  // This currentdata store which array to show on the screen
 
   return (
     <div>
       <div className="flex gap-5">
         {TopSreamsList.map((item) => {
-            // This the list of stremas like management ,laws,medical engineering  etc....
-            // updateData is the function which updates the current data after the click and updateData is called children components
+          // This the list of stremas like management ,laws,medical engineering  etc....
+          // updateData is the function which updates the current data after the click and updateData is called children components
+          // eslint-disable-next-line react/jsx-key
           return <ItemButton data={item} updateData={setCurrentData} />;
         })}
       </div>
       <div>
-        
         {TopStreamsCollegeData[currentData].map((item) => {
-            // This top college data of that strems
-           return <ItemList data={item} />;
+          // This top college data of that strems
+          return <ItemList data={item} />;
         })}
       </div>
     </div>
@@ -29,38 +28,36 @@ const TopStreamsCollege = () => {
 };
 
 const ItemButton = ({ data, updateData }) => {
-  return <button onClick={() => updateData(data.id)}
-  className="text-sm border px-2 py-1 rounded-md">
-    <h2 className="flex">
-      <BsDot className="text-2xl mt-0"/>  {data.name}
-    </h2>
-  </button>;
+  return (
+    <button
+      onClick={() => updateData(data.id)}
+      className="text-sm border px-2 py-1 rounded-md"
+    >
+      <h2 className="flex">
+        <BsDot className="text-2xl mt-0" /> {data.name}
+      </h2>
+    </button>
+  );
 };
 
-
 const ItemList = ({ data }) => {
-  
   return (
     <div>
-     {/* upper div  */}
+      {/* upper div  */}
       <div>
-       <div>
+        <div>
           <h1>{data.name}</h1>
-          <img src={data.logo}
-           className="h-10 w-10 "/>
-          <img src={data.cover_image}
-           className="h-10 w-10 "/>
-         <p>
+          <img src={data.logo} className="h-10 w-10 " />
+          <img src={data.cover_image} className="h-10 w-10 " />
+          <p>
             {data.city}, {data.state}
           </p>
-       </div>
-      
+        </div>
       </div>
-
 
       {/* below div */}
       <div className="bg-[#f7f7f7]">
-          {/* <p> {fees.fee}</p> */}
+        {/* <p> {fees.fee}</p> */}
         {/* Iterate over the fees array and display fee information */}
         {/* {data.fees.map((fee) => (
          <div>
@@ -68,7 +65,6 @@ const ItemList = ({ data }) => {
          </div>
         ))} */}
       </div>
-      
     </div>
   );
 };
--- src/routes/CollegeList.jsx
+++ src/routes/CollegeList.jsx
@@ -1,13 +1,133 @@
-import {useParams} from "react-router-dom"
+import { useParams } from "react-router-dom";
+import axios from "axios";
+import { useState, useEffect, useRef } from "react";
+import { Apiurl } from "../Data/ApiData";
 
-const CollegeList = ()=>{
-    // The id store url name and help for the fetch information for api 
-    const {id} = useParams()
-    return (
-        <>
-           <h1>{id}</h1>
-        </>
-    )
-}
+const CollegeList = () => {
+  const { id } = useParams();
+  const [collegeListData, setCollegeListData] = useState([]);
+  const [pageNumber, setPageNumber] = useState(0);
+  const [loading, setLoading] = useState(false);
+  const [title, setTitle] = useState("");
+  const containerRef = useRef(null);
+  const prevScrollY = useRef(0);
 
-export default CollegeList
+  useEffect(() => {
+    fetchData();
+  }, [pageNumber]); // Fetch data when page number changes
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY < prevScrollY.current) {
+        // User is scrolling up
+        return;
+      }
+      prevScrollY.current = currentScrollY;
+      const { scrollTop, clientHeight, scrollHeight } = containerRef.current;
+      if (scrollHeight - scrollTop <= clientHeight) {
+        if (!loading) {
+          setPageNumber((prevPageNumber) => prevPageNumber + 1);
+        }
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, [loading]); // Listen for scroll events and trigger page number increment when reaching bottom
+
+  const fetchData = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(
+        Apiurl + "college_list/" + id + "?page=" + pageNumber
+      );
+      setTitle(response.data?.leadFormTitle);
+      setCollegeListData((prevData) => [
+        ...prevData,
+        ...response.data.colleges,
+      ]);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setCollegeListData([]);
+      setLoading(false);
+    }
+  };
+  return (
+    <div
+      className="container mx-auto mt-4 max-w-screen w-full px-4"
+      ref={containerRef}
+    >
+      <h1 className="text-2xl font-bold mb-4">{title}</h1>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+        {collegeListData.map((item, index) => (
+          <CollegeListItem data={item} key={index} />
+        ))}
+      </div>
+      {loading && <p className="text-center mt-4">Loading...</p>}
+    </div>
+  );
+};
+
+const CollegeListItem = ({ data }) => {
+  return (
+    <div className="border border-gray-200 rounded-lg overflow-hidden  hover:shadow-lg shadow-md">
+      <img
+        className="w-full h-64 object-cover"
+        src={`https://static.zollege.in/${data?.cover}`}
+        alt={data?.college_name}
+      />
+      <div className="p-4">
+        <div className="flex items-center mb-2">
+          <img
+            className="w-12 h-12 rounded-full mr-3"
+            src={`https://static.zollege.in/${data?.logo}`}
+            alt={data?.college_name}
+          />
+          <div>
+            <h2 className="text-md  font-bold">{data?.college_name}</h2>
+            <p className="text-sm font-light">
+              {data?.college_city}, {data?.state}
+              <span>
+                {data?.approvals?.map((item, index) => (
+                  <span key={index}> {item}</span>
+                ))}
+              </span>
+            </p>
+          </div>
+        </div>
+        <div className="flex justify-between items-center gap-2 mb-2">
+          <div>
+            <h3 className="font-semibold">Course Fees</h3>
+            {data?.fees?.map((item, index) => (
+              <div key={index}>
+                <span>{item.short_form}</span>
+                <span className="ml-2">{item.fee}</span>
+              </div>
+            ))}
+          </div>
+          <button className="bg-blue-500 text-white px-2 py-1 rounded-lg">
+            Apply Now
+          </button>
+        </div>
+        <div>
+          <h3 className="font-semibold">
+            <div>
+              {data?.rankingData[0] && (
+                <h3 className="font-semibold">
+                  The {data?.rankingData[0]?.agency} ranking{" "}
+                  {data?.rankingData[0]?.rankingofCollege} out of{" "}
+                  {data?.rankingData[0]?.rankingOutOfTotalNoOfCollege} in{" "}
+                  {data?.rankingData[0]?.year}
+                </h3>
+              )}
+            </div>
+          </h3>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CollegeList;

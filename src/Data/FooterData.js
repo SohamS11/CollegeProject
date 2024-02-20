97f841718c1fbb36b5a89d3d8ac8ede4d987d66a
Tochage143 <rbactionmovies@gmail.com>
2024-02-20 15:48:04 +0530

    add routes navigation to the footer

---
+++ src/Data/FooterData.js
@@ -0,0 +1,67 @@
+const FooterData = [
+  {
+    title: "Top Colleges",
+    id: "top-colleges",
+    subMenu: [
+      { name: "M.B.A", id: "mba-colleges" },
+      { name: "B.Tech/B.E", id: "btech-colleges" },
+      { name: "MCA", id: "mca-colleges" },
+      { name: "BCA", id: "bca-colleges" },
+      { name: "M.Tech", id: "mtech-colleges" },
+      { name: "MA", id: "ma-colleges" },
+      { name: "BA", id: "ba-colleges" },
+    ],
+  },
+  {
+    title: "Top Universities",
+    id: "top-universities",
+    subMenu: [
+      { name: "Engineering", id: "engineering-colleges" },
+      { name: "Management", id: "management-colleges" },
+      { name: "Medical", id: "medical-colleges" },
+      { name: "Law", id: "law-colleges" },
+      { name: "Commerce", id: "commerce-colleges" },
+      { name: "Science", id: "science-colleges" },
+      { name: "Arts", id: "arts-colleges" },
+    ],
+  },
+  {
+    title: "Top Exam",
+    id: "top-exam",
+    subMenu: [
+      { name: "CAT", id: "cat" },
+      { name: "GATE", id: "exams/gate" },
+      { name: "JEE-MAIN", id: "exams/jee-main" },
+      { name: "NEET", id: "exams/neet" },
+      { name: "XAT", id: "exams/xat" },
+      { name: "CLAT", id: "exams/clat" },
+      { name: "MAT", id: "exams/mat" },
+    ],
+  },
+  {
+    title: "Top Cities",
+    id: "top-cities",
+    subMenu: [
+      { name: "Bangalore", id: "bangalore-colleges" },
+      { name: "Lucknow", id: "lucknow-colleges" },
+      { name: "Indore", id: "indore-colleges" },
+      { name: "Delhi", id: "delhi-ncr-colleges" },
+      { name: "Mumbai", id: "mumbai-colleges" },
+      { name: "Kolkata", id: "kolkata-colleges" },
+    ],
+  },
+  {
+    title: "Top States",
+    id: "top-states",
+    subMenu: [
+      { name: "Uttar Pradesh", id: "uttar-pradesh-colleges" },
+      { name: "Tamil Nadu", id: "tamil-nadu-colleges" },
+      { name: "Karnataka", id: "karnataka-colleges" },
+      { name: "West Bengal", id: "west-bengal-colleges" },
+      { name: "Kerala", id: "kerala-colleges" },
+      { name: "Maharashtra", id: "maharashtra-colleges" },
+    ],
+  },
+];
+
+export default FooterData;
--- src/component/Home/Footer.jsx
+++ src/component/Home/Footer.jsx
@@ -1,190 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import FacebookIcon from "../../assets/social-icon/facebook-icon.svg";
 import instagramIcon from "../../assets/social-icon/instagram-icon.svg";
 import linkdenIcon from "../../assets/social-icon/linkedin-icon.svg";
 import twitterIcon from "../../assets/social-icon/twitter-icon.svg";
 import githubIcon from "../../assets/social-icon/github-icon.svg";
+import FooterData from "../../Data/FooterData";
+import { useParams, useNavigate } from "react-router-dom";
 
 const Footer = () => {
+  const [itemNumber, setItemNumber] = useState(5);
+
+  useEffect(() => {
+    function Getsizewindows() {
+      if (window.innerWidth <= 640) {
+        setItemNumber(3);
+      } else {
+        setItemNumber(5);
+      }
+    }
+    window.addEventListener("resize", Getsizewindows);
+    Getsizewindows();
+  }, []);
   return (
     <div className="bg-[#f4f4f4] p-5">
-      {/* first section  */}
-
-      <div className=" flex justify-around  ">
-        {/*first column  */}
-        <div className=" mt-5  ">
-          <h2 className="font-bold ">Top Colleges </h2>
-
-          <ul className="mt-2">
-            <li>
-             
-              <a href="#">M.B.A </a>
-            </li>
-            <li>
-             
-              <a href="#">B.Tech/B.E </a>
-            </li>
-            <li>
-              <a href="#">MCA </a>
-            </li>
-            <li>
-              <a href="#"> BCA </a>
-            </li>
-            <li>
-              <a href="#"> M.Tech </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#"> MA </a>
-            </li>
-            <li>
-              <a href="#">BA </a>
-            </li>
-          </ul>
-        </div>
-        {/*second column  */}
-        <div className=" mt-5  ">
-          <h2 className="font-bold ">Top Universities </h2>
-
-          <ul className="mt-2">
-            <li>
-              <a href="#">Engineering </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#">Management </a>
-            </li>
-            <li>
-              <a href="#">Medical </a>
-            </li>
-            <li>
-              <a href="#"> Law </a>
-            </li>
-            <li>
-              <a href="#"> Commerce </a>
-            </li>
-            <li>
-              <a href="#"> science </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#">Arts </a>
-            </li>
-          </ul>
-        </div>
-
-        {/*third column  */}
-        <div className=" mt-5  ">
-          <h2 className="font-bold ">Top Exams </h2>
-
-          <ul className="mt-2">
-            <li>
-              {" "}
-              <a href="#">CAT </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#">GATE </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#">Jee Main </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#"> NEET </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#"> XAt </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#"> CLAT</a>
-            </li>
-            <li>
-              {" "}
-              <a href="#">MAT </a>
-            </li>
-          </ul>
-        </div>
-        {/*fourth column  */}
-        <div className=" mt-5  ">
-          <h2 className="font-bold "> Board Exams </h2>
-
-          <ul className="mt-2">
-            <li>
-              {" "}
-              <a href="#">CBSE Class 12 </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#">CBSE Class 12th result </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#">CBSE Class 12 th syllabus </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#"> CBSE Class 12th exam date </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#"> CBSE Class 10 </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#"> CBSE Class 10th result </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#">CBSE Class 10th syllabus</a>
-            </li>
-          </ul>
-        </div>
-        {/*fifth column  */}
-        <div className=" mt-5  ">
-          <h2 className="font-bold "> Top States </h2>
-
-          <ul className="mt-2">
-            <li>
-              {" "}
-              <a href="#"> Maharastra </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#"> UttarPradesh </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#"> Tamil Nadu </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#">Karnataka </a>
-            </li>
-            <li>
-              
-              <a href="#"> West Bengal </a>
-            </li>
-            <li>
-              {" "}
-              <a href="#"> Kerala </a>
-            </li>
-          </ul>
-        </div>
+      <div className=" flex justify-around">
+        {FooterData.slice(0, itemNumber).map((item, index) => {
+          return <FooterItem data={item} key={index} />;
+        })}
       </div>
-
-      {/* seecond section  */}
       <div className="w-full flex flex-col justify-center items-center mt-10 mb-10">
         <div className="w-full mt-5 h-0.5 bg-black opacity-25 "> </div>
-
         <div className="flex flex-col items-center mt-10 ">
           <div className=" text-black opacity-60 font-bold">
             Contact With Us
           </div>
-
           <div className="flex flex-row space-x-4 mt-2 pb-10">
             <a href="https://www.facebook.com/">
               <img
@@ -226,9 +75,7 @@ const Footer = () => {
             </a>
           </div>
         </div>
-
         <div className="w-full h-0.5 bg-black opacity-25 "> </div>
-
         <div>© 2024 MY COLLEGE Web Pvt. Ltd. All Rights Reserved</div>
       </div>
     </div>
@@ -236,3 +83,31 @@ const Footer = () => {
 };
 
 export default Footer;
+
+const FooterItem = ({ data }) => {
+  const navigate = useNavigate();
+
+  function RouteHandle(routename) {
+    if (data.id === "top-exam") {
+      navigate(`/exam/${encodeURIComponent(routename)}`);
+    } else {
+      navigate(`/collegelist/${encodeURIComponent(routename)}`);
+    }
+  }
+  return (
+    <>
+      <div className="mt-5">
+        <h2 className="font-bold">{data.title}</h2>
+        <ul className="mt-2">
+          {data.subMenu.map((item, index) => {
+            return (
+              <li key={index} className=" cursor-pointer">
+                <a onClick={() => RouteHandle(item.id)}>{item.name}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};

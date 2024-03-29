038cbc32d7d80386e56667aaf70116a3bf426c37
Tochage143 <rbactionmovies@gmail.com>
2024-03-29 17:09:03 +0530

    issue solve

--- src/Theme/Color.js
+++ src/Theme/Color.js
@@ -2,7 +2,7 @@ const Color = {
   dark: {
     mainbg: "bg-gray-900 text-gray-200", // Dark background with light text
     footer: "bg-gray-800 p-5 text-gray-300", // Dark footer with lighter text
-    card: "bg-gray-800 text-gray-100", // Dark card background with light text
+    card: "bg-gray-700 text-gray-100", // Dark card background with light text
     card2: "bg-indigo-800", // Dark secondary card background
     text: "text-gray-200", // Light text color
     hoverbg: "hover:bg-gray-700", // Dark hover background
--- src/routes/AboutUs.jsx
+++ src/routes/AboutUs.jsx
@@ -4,43 +4,80 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBullseye, faEye } from "@fortawesome/free-solid-svg-icons";
 
 const AboutSection = () => {
-  const { toggleDarkMode, darkMode } = useThemeContext();
+  const { darkMode } = useThemeContext();
 
   return (
-    <div className={`bg-${darkMode ? 'gray-800' : 'gradient-to-r from-blue-500 to-gray-200'} py-20 px-10 md:px-20 lg:px-40`}>
+    <div
+      className={`bg-${
+        darkMode ? "gray-800" : "gradient-to-r from-blue-500 to-gray-200"
+      } 
+   h-screen py-20 px-10 md:px-20 lg:px-40`}
+    >
       <div className="max-w-4xl mx-auto">
-        <h2 className={`text-4xl md:text-5xl lg:text-6xl font-bold text-center mb-10 ${darkMode ? 'text-white' : 'text-gray-900'}`}>
+        <h2
+          className={`text-4xl md:text-5xl lg:text-6xl font-bold text-center mb-10 ${
+            darkMode ? "text-white" : "text-gray-900"
+          }`}
+        >
           Welcome to MyCollege.Com
         </h2>
-        <p className={`text-lg md:text-xl lg:text-2xl leading-relaxed ${darkMode ? 'text-white' : 'text-gray-800'}`}>
+        <p
+          className={`text-lg md:text-xl lg:text-2xl leading-relaxed ${
+            darkMode ? "text-white" : "text-gray-800"
+          }`}
+        >
           Welcome to My College, where excellence in education meets a vibrant
           campus life. We strive to provide world-class education and foster a
           community that values diversity, innovation, and collaboration.
         </p>
       </div>
       <div className="flex flex-col md:flex-row justify-around items-center mt-16">
-        <div className="max-w-md mx-auto mb-10 md:mb-0 bg-white rounded-lg shadow-lg overflow-hidden">
+        <div
+          className={`max-w-md mx-auto mb-10 md:mb-0 rounded-lg shadow-lg overflow-hidden ${
+            darkMode ? Color.dark.card : Color.light.card
+          }`}
+        >
           <div className="p-8">
-            <h3 className={`text-xl md:text-2xl lg:text-3xl font-semibold mb-4 ${darkMode ? 'text-white' : 'text-gray-900'}`}>
+            <h3
+              className={`text-xl md:text-2xl lg:text-3xl font-semibold mb-4 ${
+                darkMode ? "text-white" : "text-gray-900"
+              }`}
+            >
               Our Mission
             </h3>
             <div className="flex justify-center items-center mb-8">
-              <FontAwesomeIcon icon={faBullseye} size="3x" className={`text-${darkMode ? 'gray' : 'blue-500'} mr-4`} />
-              <p className={`text-gray-800 ${darkMode ? 'text-white' : ''}`}>
-                To empower students with knowledge and skills, cultivate a passion
-                get accepted by  love
+              <FontAwesomeIcon
+                icon={faBullseye}
+                size="3x"
+                className={`text-${darkMode ? "gray" : "blue-500"} mr-4`}
+              />
+              <p className={`text-gray-800 ${darkMode ? "text-white" : ""}`}>
+                To empower students with knowledge and skills, cultivate a
+                passion get accepted by love
               </p>
             </div>
           </div>
         </div>
-        <div className="max-w-md mx-auto mb-10 md:mb-0 bg-white rounded-lg shadow-lg overflow-hidden">
+        <div
+          className={`max-w-md mx-auto mb-10 md:mb-0 bg-white rounded-lg shadow-lg overflow-hidden ${
+            darkMode ? Color.dark.card : Color.light.card
+          }`}
+        >
           <div className="p-8">
-            <h3 className={`text-xl md:text-2xl lg:text-3xl font-semibold mb-4 ${darkMode ? 'text-white' : 'text-gray-900'}`}>
+            <h3
+              className={`text-xl md:text-2xl lg:text-3xl font-semibold mb-4 ${
+                darkMode ? "text-white" : "text-gray-900"
+              }`}
+            >
               Our Vision
             </h3>
             <div className="flex justify-center items-center mb-8">
-              <FontAwesomeIcon icon={faEye} size="3x" className={`text-${darkMode ? 'gray' : 'blue-500'} mr-4`} />
-              <p className={`text-gray-800 ${darkMode ? 'text-white' : ''}`}>
+              <FontAwesomeIcon
+                icon={faEye}
+                size="3x"
+                className={`text-${darkMode ? "gray" : "blue-500"} mr-4`}
+              />
+              <p className={`text-gray-800 ${darkMode ? "text-white" : ""}`}>
                 To be a leading institution in providing holistic education,
                 fostering creativity, and producing responsible global citizens.
                 dfdsfdfdsdff

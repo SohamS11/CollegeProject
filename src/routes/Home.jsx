201318b1438f61a3d0244c90cb1961ecb98272f9
SohamS11 <sohamsangles2002@gmail.com>
2024-05-01 23:18:18 +0530

    build project

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -2,9 +2,9 @@ import HeroSection from "../component/Home/Hero_Section";
 import FeaturesSection from "../component/Home/FeaturesSection";
 import Footer from "../component/Home/Footer";
 import TopCities from "../component/Home/TopCities";
-import Streams from "../Component/Home/Streams";
-import TopStreamsCollege from "../Component/Home/TopStreamsCollege";
-import SelectCourse from "../Component/Home/SelectCourse";
+import Streams from "../component/Home/Streams";
+import TopStreamsCollege from "../component/Home/TopStreamsCollege";
+import SelectCourse from "../component/Home/SelectCourse";
 import { useThemeContext } from "../ContextApi/ThemeContext";
 import Color from "../Theme/Color";
 
